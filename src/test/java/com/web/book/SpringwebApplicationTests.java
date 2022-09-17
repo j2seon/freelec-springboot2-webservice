ca97439752a82a2befe9e44e035dc55af93e7ca4
Ju Jinseon <ddj04323@gmail.com>
2022-09-18 02:40:56 +0900

    Initial commit

---
+++ src/test/java/com/web/book/SpringwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
